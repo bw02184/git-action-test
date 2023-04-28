e55da42c1da75a02106e1c006a43f2a15fee0671
bw02184 <bw02184@naver.com>
2023-04-28 18:21:08 +0900

    cache 속도 확인 Test2

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,37 +4,37 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class MemberService {
-    private final MemberRepository memberRepository;
+  private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
-    //회원가입
-    public Long join(Member member) {
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
+  // 회원가입
+  public Long join(Member member) {
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
 
-    private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-            .ifPresent(m -> {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
+  private void validateDuplicateMember(Member member) {
+    memberRepository
+        .findByName(member.getName())
+        .ifPresent(
+            m -> {
+              throw new IllegalStateException("이미 존재하는 회원입니다..");
             });
-    }
+  }
 
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
 
-    public Optional<Member> findOne(Long memberId){
-        return memberRepository.findById(memberId);
-    }
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
 }
